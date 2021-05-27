fc9079d561430edd91e3693cfe24c2086604633c
claudesortwell <41422239+claudesortwell@users.noreply.github.com>
2021-05-27 11:43:14 +1000

    Inital Commit from stage 1

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# COMP3100
+# Simple Job Dispatcher for Distributed Systems
+
+Developed for the COMP3100 unit at Macquarie University by:
+* **Bharosha Poudel** – 45705879
+* **Claude Sortwell** – 45634947
+* **Nathan Lecompte** – 45423725
+
+### About this Project:
+This project involves the design and development of a simple job scheduler for a simulated
+distributed system. The key parts responsible for facilitating this client-server simulation can be
+separated into two components: a client-side simulator and a server-side simulator. This document
+describes stage one of the project, which involves the design and development of a simple
+client-side simulator which can communicate using the ‘ds-sim’ simulation protocol and dispatch
+jobs using the ‘allToLargest’ algorithm (jobs are dispatched to the server with the largest core
+count).
+
+### Important Files:
+* `./COMP3100 - Group 44 - Simple Job Dispatcher for Distributed Systems.pdf` - A detailed report on the design and implementation of the simple job dispatcher.
+* `/src/Client.java` - The client-side simulator developed during the project.
---
+++ src/Client.java
@@ -0,0 +1,195 @@
+import java.net.*;
+import java.io.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class Client {
+
+// Server class required to find a server.
+    private class Server {
+        public String type;
+        public int limit;
+        public int bootupTime;
+        public float hourlyRate;
+        public int coreCount;
+        public int memory;
+        public int disk;
+
+        public Server(String type, int limit, int bootupTime, float hourlyRate, int coreCount, int memory, int disk) {
+            this.type = type;
+            this.limit = limit;
+            this.bootupTime = bootupTime;
+            this.hourlyRate = hourlyRate;
+            this.coreCount = coreCount;
+            this.memory = memory;
+            this.disk = disk;
+        }
+    }
+
+    private static Socket socket;
+    private static BufferedReader inputBuffer;
+    private static DataInputStream inputStream;
+    private static DataOutputStream outputStream;
+
+    public Client() {
+        try {
+            socket = new Socket("localhost", 50000);
+            inputStream = new DataInputStream(socket.getInputStream());
+            inputBuffer = new BufferedReader(new InputStreamReader(inputStream));
+            outputStream = new DataOutputStream(socket.getOutputStream());
+            System.out.println("Client initialized!");
+        } catch (Exception e) {
+            System.out.println("Error: Client failed to initialize");
+        }
+    }
+
+    public static void main(String args[]) throws Exception {
+        String output = "";
+        Client client = new Client();
+
+        output = client.sendMessage("HELO");
+        System.out.printf("RCVD %s \n", output);
+        output = client.sendMessage("AUTH " + System.getProperty("user.name"));
+        System.out.printf("RCVD %s \n", output);
+        output = client.sendMessage("REDY");
+        System.out.printf("RCVD %s \n", output);
+
+        if (!output.contains("NONE")) {
+            boolean processing = true;
+            Server[] servers = client.getServers("ds-system.xml");
+            Server largestServer = client.getLargestServer(servers);
+            while (processing) {
+                if (output.contains("OK")) {
+                    output = client.sendMessage("REDY");
+                    System.out.printf("RCVD %s \n", output);
+                }
+                if (output.contains("NONE") || output.contains("ERR")) {
+                    processing = false;
+                    break;
+                }
+                if (output.contains("JCPL")) {
+                    output = client.sendMessage("REDY");
+                    System.out.printf("RCVD %s \n", output);
+                } else {
+                    try {
+                        String[] jobData = output.split("\\s+");
+                        int jobCount = Integer.parseInt(jobData[2]);
+                        output = client.sendMessage("SCHD " + jobCount + " " + largestServer.type + " " + "0");
+                        System.out.printf("RCVD %s \n", output);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        }
+
+        output = client.sendMessage("QUIT");
+        if (output.contains("QUIT")) {
+            client.closeConnection();
+        }
+    }
+
+    /**
+     * Send a message to the output stream (to the server)
+     * 
+     * @param message the message to be sent
+     * @return response from the input buffer (from the server)
+     */
+    public String sendMessage(String message) {
+        String output = "";
+        try {
+        	message = message + "\n";
+            outputStream.write(message.getBytes());
+            outputStream.flush();
+            System.out.printf("SENT %s \n", message);
+            output = getResponse();
+        } catch (Exception e) {
+            System.out.println("Error: Client failed to send message");
+        }
+        return output;
+    }
+
+    /**
+     * Listen for a message from the input buffer (from the server)
+     * 
+     * @return the response from the input buffer (from the server)
+     */
+    public String getResponse() {
+        String message = "";
+
+        try {
+            while (!inputBuffer.ready()) {
+            }
+            while (inputBuffer.ready()) {
+                message += (char) inputBuffer.read();
+            }
+        } catch (Exception e) {
+            System.out.println("Error: Client failed to receive message");
+        }
+
+        return message;
+    }
+
+    /**
+     * Determine the largest server
+     */
+    public Server getLargestServer(Server[] servers) {
+        Server largestServer = servers[0];
+        for (int i = 1; i < servers.length; i++) {
+            if (largestServer.coreCount < servers[i].coreCount) {
+                largestServer = servers[i];
+            }
+        }
+
+        return largestServer;
+    }
+
+    /**
+     * Closes the connection
+     */
+    public void closeConnection() {
+        try {
+            outputStream.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println("Error: Client failed to close connection");
+        }
+    }
+
+    /**
+     * Retrieves all the servers available in the given config path
+     * 
+     * @param configPath path to the XML file which holds the config for the servers
+     * @return array of server objects contained in the config file
+     */
+    public Server[] getServers(String configPath) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document config = builder.parse(new File(configPath));
+            config.getDocumentElement().normalize();
+
+            NodeList serverNodeList = config.getElementsByTagName("server");
+            Server[] servers = new Server[serverNodeList.getLength()];
+
+            for (int i = 0; i < serverNodeList.getLength(); i++) {
+                Element server = (Element) serverNodeList.item(i);
+                servers[i] = new Server(server.getAttribute("type"), Integer.parseInt(server.getAttribute("limit")),
+                        Integer.parseInt(server.getAttribute("bootupTime")),
+                        Float.parseFloat(server.getAttribute("hourlyRate")),
+                        Integer.parseInt(server.getAttribute("coreCount")),
+                        Integer.parseInt(server.getAttribute("memory")), Integer.parseInt(server.getAttribute("disk")));
+            }
+
+            return servers;
+        } catch (Exception e) {
+            System.out.println("Error: Client failed to get servers from config");
+        }
+
+        return new Server[0];
+    }
+}
